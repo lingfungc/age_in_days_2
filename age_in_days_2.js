df04a0604126ab5f245a971dd13b3a26b6be8a99
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-08 16:17:11 +0000

    completed ageInDays function and added psuedocode

--- age_in_days_2.js
+++ age_in_days_2.js
@@ -1,3 +1,11 @@
-const ageInDays = (year, month, year) => {
+const ageInDays = (year, month, day) => {
+  const today = Date.now();
+  const birthDay = new Date(year, month - 1, day);
+  return Math.floor((today - birthDay) / (1000 * 60 * 60 * 24));
+};
 
-}
+// Need to use syntax new Date to get the date
+// Month argument in JavaScript is starting from 0 - 11
+
+console.log(ageInDays(1997, 6, 30));
+console.log(ageInDays(2022, 11, 7));
