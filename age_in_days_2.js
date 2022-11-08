b31c1fbe0e59f86a3c01286e496a70ade2b9a1db
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-08 16:22:08 +0000

    added alternative logic function

--- age_in_days_2.js
+++ age_in_days_2.js
@@ -1,5 +1,5 @@
 const ageInDays = (year, month, day) => {
-  const today = Date.now();
+  const today = new Date();
   const birthDay = new Date(year, month - 1, day);
   return Math.floor((today - birthDay) / (1000 * 60 * 60 * 24));
 };
@@ -9,3 +9,21 @@ const ageInDays = (year, month, day) => {
 
 console.log(ageInDays(1997, 6, 30));
 console.log(ageInDays(2022, 11, 7));
+
+// const date = new Date();
+
+// let day = date.getDate();
+// let month = date.getMonth() + 1;
+// let year = date.getFullYear();
+
+// This arrangement can be altered based on how we want the date's format to appear.
+
+// let currentDate = `${day}-${month}-${year}`;
+// console.log(currentDate); // "17-6-2022"
+
+// Alternative logic
+
+// const ageInDays = (day, month, year) => {
+//   const birthday = new Date(year, month - 1, day);
+//   return Math.round((Date.now() - birthday) / 86400000);
+// };
